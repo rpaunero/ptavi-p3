7fab905cafbb3f01efc2caaf5c6bef8af89632f7
rpaunero <r.paunero@alumnos.urjc.es>
2015-10-19 11:31:50 +0200

    Correccion

--- karaoke.py
+++ karaoke.py
@@ -6,14 +6,25 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import smallsmilhandler
 
-Lector = smallsmilhandler.SmallSMILHandler
+
+def crearlinea(misDatos):
+    linea=''
+    for sublista in misDatos:
+        linea = linea + sublista[0]
+        dicc = sublista[1]
+        for atributo in dicc:      
+            if dicc[atributo] != "":
+                linea = linea + "\t" + atributo + "=" + (dicc[atributo] + " ")
+    print(linea)
 
 if __name__ == "__main__":
     try:
         fichero = sys.argv[1]
     except IndexError:
-        sys.exit("Usage: python3 karaoke.py file.smil.")
+        sys.exit("Usage: python3 karaoke.py file.smil")
     parser = make_parser()
     cHandler = smallsmilhandler.SmallSMILHandler()
     parser.setContentHandler(cHandler)
-    parser.parse(open('karaoke.smil'))
+    parser.parse(open(fichero)) 
+    misDatos = cHandler.get_tags()
+    crearlinea(misDatos)
