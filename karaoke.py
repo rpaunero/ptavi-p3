93d47a1e7c07b8a85bc69ed90b3edf4e34686c6d
rpaunero <r.paunero@alumnos.urjc.es>
2015-10-19 12:40:27 +0200

    Terminado: clase KaraokeLocal

--- karaoke.py
+++ karaoke.py
@@ -7,34 +7,41 @@ from xml.sax.handler import ContentHandler
 import smallsmilhandler
 import urllib
 
+class KaraokeLocal():
 
-def __str__(misDatos):
-    linea=''
-    for sublista in misDatos:
-        linea = linea + sublista[0]
-        dicc = sublista[1]
-        for atributo in dicc:      
-            if dicc[atributo] != "":
-                linea = linea + "\t" + atributo + "=" + (dicc[atributo] + " ")
-    print(linea)
+    def __init__(self, fichero):
+        parser = make_parser()
+        cHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(fichero)) 
+        self.misDatos = cHandler.get_tags()
+    
+    def __str__(self):
+        linea=''
+        for sublista in self.misDatos:
+            linea = linea + sublista[0]
+            dicc = sublista[1]
+            for atributo in dicc:      
+                if dicc[atributo] != "":
+                    linea = linea + "\t" + atributo + "=" + (dicc[atributo] + " ")
+        return (linea)
 
-def do_local(misDatos):
-    for sublista in misDatos:
-        dicc = sublista[1]
-        for atributo in dicc:
-            if atributo == 'src':
-                if dicc[atributo] != "http://": 
-                    urllib.request.urlretrieve(dicc[atributo],dicc[atributo].split('/')[-1])
+    def do_local(self):
+        for sublista in self.misDatos:
+            dicc = sublista[1]
+            for atributo in dicc:
+                if atributo == 'src':
+                    if dicc[atributo].split('/')[0] == "http:":
+                        urllib.request.urlretrieve(dicc[atributo],dicc[atributo].split('/')[-1])
+                        dicc[atributo] = dicc[atributo].split('/')[-1]
 
 
 if __name__ == "__main__":
     try:
         fichero = sys.argv[1]
+        karaoke = KaraokeLocal(fichero)
+        print(karaoke)
+        karaoke.do_local()
+        print(karaoke)
     except IndexError:
         sys.exit("Usage: python3 karaoke.py file.smil")
-    parser = make_parser()
-    cHandler = smallsmilhandler.SmallSMILHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(open(fichero)) 
-    misDatos = cHandler.get_tags()
-    crearlinea(misDatos)
